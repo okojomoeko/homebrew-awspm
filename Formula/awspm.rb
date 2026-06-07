1bad8a933850d2e6a17ab21cfb57735165cce4fc
okojomoeko <chikoro.size@gmail.com>
2026-06-07 23:38:47 +0900

    refactor: use version interpolation for URLs

--- Formula/awspm.rb
+++ Formula/awspm.rb
@@ -4,23 +4,23 @@ class Awspm < Formula
 
  homepage "https://github.com/okojomoeko/awspm"
 
- version "0.1.0"
+ version "0.1.1"
 
  if OS.mac? && Hardware::CPU.intel?
 
-   url "https://github.com/okojomoeko/awspm/releases/download/v0.1.1/awspm-macos-amd64.tar.gz"
+   url "https://github.com/okojomoeko/awspm/releases/download/v#{version}/awspm-macos-amd64.tar.gz"
 
    sha256 "20787dbc5ff15b981fe5f55c54bdd790aca69eae7f37b62f0ba6119773a15386"
 
  elsif OS.mac? && Hardware::CPU.arm?
 
-   url "https://github.com/okojomoeko/awspm/releases/download/v0.1.1/awspm-macos-arm64.tar.gz"
+   url "https://github.com/okojomoeko/awspm/releases/download/v#{version}/awspm-macos-arm64.tar.gz"
 
    sha256 "050973b95cb1a0d84f28f181e4a05d567d278537e289c5eeb7896e747357ffe3"
 
  elsif OS.linux? && Hardware::CPU.intel?
 
-   url "https://github.com/okojomoeko/awspm/releases/download/v0.1.1/awspm-linux-amd64.tar.gz"
+   url "https://github.com/okojomoeko/awspm/releases/download/v#{version}/awspm-linux-amd64.tar.gz"
 
    sha256 "53a05e02a1c325e15f700a79ce9d1bbf07b70c02e61512b1320f51c8c8318579"
 
